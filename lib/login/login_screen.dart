6683d23f96e081da280780cefc75bc8919e54611
malakalabttire <malakalbattire@Malaks-MacBook-Air.local>
2024-06-07 19:05:59 +0300

    Spinner and SnackBar

--- lib/login/login_screen.dart
+++ lib/login/login_screen.dart
@@ -3,6 +3,7 @@ import 'package:animation_flutter/shared/roundedButton.dart';
 import 'package:animation_flutter/home/home.dart';
 import 'package:animation_flutter/shared/constants.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 class LoginScreen extends StatefulWidget {
   static const String id = 'login_screen';
@@ -15,66 +16,85 @@ class _LoginScreenState extends State<LoginScreen> {
   final _auth = FirebaseAuth.instance;
   late String email;
   late String password;
+  bool showSpinner = false;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: EdgeInsets.all(30.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Hero(
-                  tag: 'logo',
-                  child: Container(
-                    height: 200.0,
-                    child: Image.asset('images/logo.png'),
+    return ModalProgressHUD(
+      inAsyncCall: showSpinner,
+      child: Scaffold(
+        body: SafeArea(
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: EdgeInsets.all(30.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Hero(
+                    tag: 'logo',
+                    child: Container(
+                      height: 200.0,
+                      child: Image.asset('images/logo.png'),
+                    ),
                   ),
-                ),
-                SizedBox(
-                  height: 20.0,
-                ),
-                TextField(
-                  keyboardType: TextInputType.emailAddress,
-                  onChanged: (value) {
-                    email = value;
-                  },
-                  decoration: kTextFieldDecoration.copyWith(
-                      hintText: 'Enter Your Email.'),
-                ),
-                kSizedBox20,
-                TextField(
-                  obscureText: true,
-                  onChanged: (value) {
-                    password = value;
-                  },
-                  decoration: kTextFieldDecoration.copyWith(
-                    hintText: 'Enter your password.',
+                  SizedBox(
+                    height: 20.0,
                   ),
-                ),
-                kSizedBox30,
-                RoundedButton(
-                    title: 'Log In ',
-                    colour: Colors.lightBlueAccent,
-                    onPressed: () async {
-                      // setState(() {
-                      //   showSpinner = true;
-                      // });
-                      try {
-                        final user = await _auth.signInWithEmailAndPassword(
-                            email: email, password: password);
-                        if (user != null) {
-                          Navigator.pushNamed(context, Home.id);
+                  TextField(
+                    keyboardType: TextInputType.emailAddress,
+                    onChanged: (value) {
+                      email = value;
+                    },
+                    decoration: kTextFieldDecoration.copyWith(
+                        hintText: 'Enter Your Email.'),
+                  ),
+                  kSizedBox20,
+                  TextField(
+                    obscureText: true,
+                    onChanged: (value) {
+                      password = value;
+                    },
+                    decoration: kTextFieldDecoration.copyWith(
+                      hintText: 'Enter your password.',
+                    ),
+                  ),
+                  kSizedBox30,
+                  RoundedButton(
+                      title: 'Log In ',
+                      colour: Colors.lightBlueAccent,
+                      onPressed: () async {
+                        setState(() {
+                          showSpinner = true;
+                        });
+                        try {
+                          final user = await _auth.signInWithEmailAndPassword(
+                              email: email, password: password);
+                          if (user != null) {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(
+                                content: Text('logged in successfully '),
+                                backgroundColor: Colors.green,
+                              ),
+                            );
+                            Navigator.pushNamed(context, Home.id);
+                          }
+                          setState(() {
+                            showSpinner = false;
+                          });
+                        } catch (e) {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(
+                              content: Text(e.toString()),
+                              backgroundColor: Colors.red,
+                            ),
+                          );
                         }
-                        // setState(() {
-                        //   showSpinner = false;
-                        // });
-                      } catch (e) {
-                        print(e);
-                      }
-                    }),
-              ],
+                        setState(() {
+                          showSpinner = false;
+                        });
+                      }),
+                ],
+              ),
             ),
           ),
         ),
